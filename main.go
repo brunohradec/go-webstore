81f23ac4e068fa5d8295ed9cdc728a8477bee3fa
Bruno Hradec <bhradec@gmail.com>
2023-08-15 18:04:33 +0200

    Remove test users and items

--- main.go
+++ main.go
@@ -9,59 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID       int64  `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	LastName string `json:"lastName"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type Item struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Price       int64  `json:"price"`
-	SellerID    int64  `json:"sellerID"`
-}
-
-var users = []User{
-	{
-		ID:       1,
-		Username: "iivic",
-		Name:     "Ivan",
-		LastName: "Ivic",
-		Email:    "iivic@test.com",
-		Password: "iivicpass",
-	},
-	{
-		ID:       2,
-		Username: "pperic",
-		Name:     "Pero",
-		LastName: "Peric",
-		Email:    "pperic@test.com",
-		Password: "ppericpass",
-	},
-}
-
-var items = []Item{
-	{
-		ID:          1,
-		Title:       "Jabuka",
-		Description: "Jabuka Granny Smith",
-		Price:       200,
-		SellerID:    1,
-	},
-	{
-		ID:          2,
-		Title:       "Kruska",
-		Description: "Kruska Williams",
-		Price:       250,
-		SellerID:    1,
-	},
-}
-
 func main() {
 	env, err := initializers.LoadDotenvVariables()
 
@@ -91,10 +38,6 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
-	})
-
-	r.GET("/items", func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, items)
 	})
 
 	r.Run(":" + env.Port)
